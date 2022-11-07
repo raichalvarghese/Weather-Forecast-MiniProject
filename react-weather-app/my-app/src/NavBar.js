327df1236ebc05f9ca0e35b3484985622e93dfda
Raichal <raichalvarghese231@gmail.com>
2022-11-08 00:22:49 +0530

    react-app

---
+++ react-weather-app/my-app/src/NavBar.js
@@ -0,0 +1,17 @@
+export default function NavBar(){
+    return <nav className="nav">
+        <h2 className="site-title">Weather App</h2>
+        <ul>
+            <li>
+                <a href="/today">Today</a>
+
+            </li>
+            <li>
+                <a href="/hourly">Hourly</a>
+            </li>
+            <li>
+                <a href="/hourly">Hourly</a>
+            </li>
+        </ul>
+    </nav>
+}
